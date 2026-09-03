5ea9ee36aa7f59cdff68f75c9164185299c60b85
Vitalie Ciubotaru <vitalie@ciubotaru.tokyo>
2026-09-04 00:52:43 +0900

    Add 'hashmap_entry_t' struct

--- hashmap-internals.h
+++ hashmap-internals.h
@@ -1,13 +1,21 @@
 #ifndef HASHMAP_INTERNALS_H
 #define HASHMAP_INTERNALS_H
 
+typedef struct hashmap_entry {
+	void *key;
+	size_t key_size;
+	void *data;
+	size_t data_size;
+	struct hashmap_entry *next;
+} hashmap_entry_t;
+
 typedef struct {
 	size_t min_buckets;
 } hashmap_options_t;
 
 struct hashmap {
 	size_t nr_buckets;
-	void **buckets;
+	hashmap_entry_t **buckets;
 	hashmap_options_t options;
 };
 
